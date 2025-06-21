39baaa726bbc090258310062e0607ae7e4599177
krymtkts <takatoshi.kuriyama@gmail.com>
2025-06-21 17:02:08 +0900

    Refactor UI mode handling in `StartGameOfLifeCommand` to use DU.

--- src/PSGameOfLife/Library.fs
+++ src/PSGameOfLife/Library.fs
@@ -3,6 +3,50 @@
 open System
 open System.Management.Automation
 
+[<AutoOpen>]
+module Data =
+    open System.Collections
+    open Microsoft.FSharp.Reflection
+
+    let generateArrayOfDu aType = FSharpType.GetUnionCases aType
+
+    let makeUnion<'DU> (u: UnionCaseInfo) = FSharpValue.MakeUnion(u, [||]) :?> 'DU
+
+    let generateDictOfDu<'DU> () =
+        let dict = Generic.Dictionary<string, 'DU> StringComparer.InvariantCultureIgnoreCase
+
+        generateArrayOfDu typeof<'DU>
+        |> Array.fold
+            (fun (acc: Generic.Dictionary<string, 'DU>) u ->
+                acc.Add(u.Name, makeUnion<'DU> u)
+                acc)
+            dict
+
+    let tryGetDu (dict: Generic.Dictionary<string, 'DU>) s =
+        let aType: Type = typeof<'DU>
+
+        match dict.TryGetValue s with
+        | true, du -> Ok du
+        | _ -> Error <| $"Unknown %s{aType.Name} '%s{s}'."
+
+    let private fromString<'DU> (dict: Generic.Dictionary<string, 'DU>) s =
+        tryGetDu dict s
+        |> function
+            | Ok x -> x
+            | Error e -> failwith e
+
+    [<RequireQualifiedAccess>]
+    [<NoEquality>]
+    [<NoComparison>]
+    [<Struct>]
+    type UIMode =
+        | Cui
+        | Gui
+
+    [<RequireQualifiedAccess>]
+    module UIMode =
+        let fromString = generateDictOfDu<UIMode> () |> fromString<UIMode>
+
 [<Cmdlet(VerbsLifecycle.Start, "GameOfLife")>]
 type StartGameOfLifeCommand() =
     inherit Cmdlet()
@@ -21,22 +65,23 @@ type StartGameOfLifeCommand() =
     member val IntervalMs = UI.Character.defaultInterval with get, set
 
     [<Parameter(Mandatory = false, HelpMessage = "UI mode for the game.")>]
-    [<ValidateSet("CUI", "GUI")>]
-    member val UI: string = "CUI" with get, set
+    [<ValidateSet("Cui", "Gui")>]
+    member val UIMode: string = "Cui" with get, set
 
     override __.BeginProcessing() = ()
     override __.ProcessRecord() = ()
 
     override __.EndProcessing() =
-        match __.UI with
-        | "CUI" ->
-            use screen = new UI.Character.Screen()
-            let initializer = Algorithm.random __.FateRoll
-            let board = Core.createBoard initializer screen.Width screen.Height __.IntervalMs
-            UI.Character.game screen board
-        | "GUI" ->
-            use screen = new UI.Avalonia.Screen()
-            let initializer = Algorithm.random __.FateRoll
-            let board = Core.createBoard initializer screen.Width screen.Height __.IntervalMs
-            UI.Avalonia.game screen board
-        | _ -> failwithf "Unknown UI mode: %s. Use 'CUI' or 'GUI'." __.UI
+        __.UIMode
+        |> UIMode.fromString
+        |> function
+            | UIMode.Cui ->
+                use screen = new UI.Character.Screen()
+                let initializer = Algorithm.random __.FateRoll
+                let board = Core.createBoard initializer screen.Width screen.Height __.IntervalMs
+                UI.Character.game screen board
+            | UIMode.Gui ->
+                use screen = new UI.Avalonia.Screen()
+                let initializer = Algorithm.random __.FateRoll
+                let board = Core.createBoard initializer screen.Width screen.Height __.IntervalMs
+                UI.Avalonia.game screen board
