ba99652f80637e5e5f223425db91a1eb401946c9
krymtkts <takatoshi.kuriyama@gmail.com>
2025-07-12 11:33:26 +0900

    Refactor FpsCounter to use `Stopwatch` for time measurement instead of `DateTime`.

--- src/PSGameOfLife/Diagnostics.fs
+++ src/PSGameOfLife/Diagnostics.fs
@@ -1,16 +1,16 @@
 module PSGameOfLife.Diagnostics
 
-open System
+open System.Diagnostics
 
 module FpsCounter =
-    let mutable lastTime = DateTime.UtcNow
+    let mutable lastTime = Stopwatch.GetTimestamp()
     let mutable frameCount = 0
     let mutable fps = 0.0
 
     let tick () =
-        let now = DateTime.UtcNow
+        let now = Stopwatch.GetTimestamp()
         frameCount <- frameCount + 1
-        let elapsed = (now - lastTime).TotalSeconds
+        let elapsed = (now - lastTime |> float) / float Stopwatch.Frequency
 
         if elapsed >= 1.0 then
             fps <- float frameCount / elapsed
